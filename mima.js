e1f8ba94ab0fab7137880f926325d046f9ef5062
109guo <212545318@qq.com>
2025-07-01 13:02:38 +0800

    Update mima.js

--- mima.js
+++ mima.js
@@ -1,7 +1,7 @@
 function mima() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username == "Yi" && password == "123") {
+    if (username == "Yi" && password == "0823") {
      window.location.href = "guoqin.html"
     } else {
         alert("非法")
